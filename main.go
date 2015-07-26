7c6cc6fc6cd5440594950af4594e7d64cb242bc5
Christoph Schmitter <cs@system32.de>
2015-07-26 23:26:54 +0200

    test changing dependencies

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ package main
 
 import (
 	"github.com/julienschmidt/httprouter"
-	"github.com/rdegges/ipify-api/api"
+	"github.com/potter0815/ipify-api/api"
 	"github.com/rs/cors"
 	"log"
 	"net/http"
